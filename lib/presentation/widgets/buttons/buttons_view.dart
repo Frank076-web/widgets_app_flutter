96481e16a9f988ca6283133cd8c5ffa92f8d1c1a
Franco <francoeinzerillo@gmail.com>
2024-10-09 12:27:12 -0300

    creado custom button

--- lib/presentation/widgets/buttons/buttons_view.dart
+++ lib/presentation/widgets/buttons/buttons_view.dart
@@ -50,11 +50,33 @@ class ButtonsView extends StatelessWidget {
                 backgroundColor: WidgetStatePropertyAll(colors.primary),
               ),
             ),
-
-            //TODO: custom button
+            const _CustomButton()
           ],
         ),
       ),
     );
+  }
+}
+
+class _CustomButton extends StatelessWidget {
+  const _CustomButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Material(
+          color: colors.primary,
+          child: InkWell(
+            onTap: () {},
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child:
+                  Text('Custom Button', style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ));
   }
 }
